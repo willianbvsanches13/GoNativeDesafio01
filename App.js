9e901461aba20e7fdbf5318d4b9d40c2efc67b65
Willian Sanches <willianbvsanches@gmail.com>
2017-11-15 20:45:24 -0200

    Adicionado ESlint e Proptypes xD

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
-  Text,
   ScrollView,
 } from 'react-native';
 
@@ -10,53 +9,42 @@ import './config/ReactotronConfig';
 import Header from './components/Header';
 import Post from './components/Post';
 
-export default class App extends Component {
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#EE7777',
+  },
+});
 
+export default class App extends Component {
   state = {
     posts: [
       {
         id: 0,
         title: 'Aprendendo React Native',
         autor: 'Willian Braga Villa Sanches',
-        text: 'Os posts devem estar armazenados no estado do componente principal da sua'+
-                'aplicação contendo título, autor e descrição de cada item. Dessa forma, os mesmos'+
-                'devem ser renderizados em tela utilizando a função map para percorrê-los',
+        text: 'Os posts devem estar armazenados no estado do componente principal da sua aplicação contendo título, autor e descrição de cada item. Dessa forma, os mesmos devem ser renderizados em tela utilizando a função map para percorrê-los',
       },
       {
         id: 1,
         title: 'Aprendendo React Native',
         autor: 'Willian Braga Villa Sanches',
-        text: 'Os posts devem estar armazenados no estado do componente principal da sua'+
-                'aplicação contendo título, autor e descrição de cada item. Dessa forma, os mesmos'+
-                'devem ser renderizados em tela utilizando a função map para percorrê-los',
+        text: 'Os posts devem estar armazenados no estado do componente principal da sua aplicação contendo título, autor e descrição de cada item. Dessa forma, os mesmos devem ser renderizados em tela utilizando a função map para percorrê-los',
       },
       {
         id: 2,
         title: 'Aprendendo React Native',
         autor: 'Willian Braga Villa Sanches',
-        text: 'Os posts devem estar armazenados no estado do componente principal da sua'+
-                'aplicação contendo título, autor e descrição de cada item. Dessa forma, os mesmos'+
-                'devem ser renderizados em tela utilizando a função map para percorrê-los',
+        text: 'Os posts devem estar armazenados no estado do componente principal da sua aplicação contendo título, autor e descrição de cada item. Dessa forma, os mesmos devem ser renderizados em tela utilizando a função map para percorrê-los',
       },
-    ]
+    ],
   };
 
   render() {
     return (
       <ScrollView style={styles.container} >
-        <Header title="GoNative App"></Header>
-        { this.state.posts.map(post => <Post key={ post.id } post={ post } />) }
+        <Header title="GoNative App" />
+        { this.state.posts.map(post => (<Post key={post.id} post={post} />)) }
       </ScrollView>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#EE7777',
-  },
-
-  post: {
-    marginTop: 20
-  }
-});
--- components/Post.js
+++ components/Post.js
@@ -1,49 +1,56 @@
-import React, { Component } from 'React'
+import React, { Component } from 'React';
 
 import {
-	View,
-	Text,
-	StyleSheet,
+  View,
+  Text,
+  StyleSheet,
 } from 'react-native';
-
-const Post = ({ post }) => (
-	<View style={styles.container}>
-		<Text style={styles.title}>{ post.title }</Text>
-		<Text style={styles.autor}>{ post.autor }</Text>
-		<Text style={styles.text} >{ post.text }</Text>
-	</View>
-);
+import PropTypes from 'prop-types';
 
 const styles = StyleSheet.create({
-	container: {
-		marginTop: 20,
-		marginLeft: 20,
-		marginRight: 20,
-		padding: 20,
-		backgroundColor: '#FFF',
-		borderRadius: 5,
-		shadowColor: '#DA6C6C',
-		shadowRadius: 5,
-	},
-
-	title: {
-		fontSize: 20,
-		fontWeight: 'bold',
-		color: '#333333',
-	},
-
-	autor: {
-		fontSize: 15,
-		color: '#999999',
-	},
-
-	text: {
-		fontSize: 15,
-		color: '#666666',
-		marginTop: 20,
-		shadowColor: '#DA6C6C',
-		shadowRadius: 5,
-	}
+  container: {
+    marginTop: 20,
+    marginLeft: 20,
+    marginRight: 20,
+    padding: 20,
+    backgroundColor: '#FFF',
+    borderRadius: 5,
+    shadowColor: '#DA6C6C',
+    shadowRadius: 5,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#333333',
+  },
+  autor: {
+    fontSize: 15,
+    color: '#999999',
+  },
+  text: {
+    fontSize: 15,
+    color: '#666666',
+    marginTop: 20,
+    shadowColor: '#DA6C6C',
+    shadowRadius: 5,
+  },
 });
+
+const Post = ({ post }) => (
+  <View style={styles.container}>
+    <Text style={styles.title}>{ post.title }</Text>
+    <Text style={styles.autor}>{ post.autor }</Text>
+    <Text style={styles.text} >{ post.text }</Text>
+  </View>
+);
+
+Post.propTypes = {
+  post: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    autor: PropTypes.string,
+    text: PropTypes.string,
+  }),
+};
 
 export default Post;
